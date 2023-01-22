7b0e01bff2912c30a2a225f859a59baa4130def7
Naveen-gali <naveen.gali@mutualmobile.com>
2023-01-22 11:38:45 +0530

    Models Created

--- App.tsx
+++ App.tsx
@@ -10,12 +10,23 @@ import {NavigationContainer} from "@react-navigation/native";
 import RNBootSplash from "react-native-bootsplash";
 import { RootNavigator } from './src/Navigation/RootNavigator';
 import {SafeAreaProvider} from "react-native-safe-area-context";
+import { store, StoreContext } from './src/models/store';
+
+const RootStore = store.create({
+  photos: [],
+  page: 0,
+  pages: 0, 
+  perpage: 0,
+  total: 0,
+});
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer onReady={() => RNBootSplash.hide({fade: true,duration: 500})}>
-        <RootNavigator />
-    </NavigationContainer>
+    <StoreContext.Provider value={RootStore}>
+      <NavigationContainer onReady={() => RNBootSplash.hide({fade: true,duration: 500})}>
+          <RootNavigator />
+      </NavigationContainer>
+    </StoreContext.Provider>
   );
 }
 
