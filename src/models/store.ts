bc8323ed917c43dcb324b2a016c56ae9e67b61db
Naveen-gali <naveen.gali@mutualmobile.com>
2023-02-14 11:21:22 +0530

    Flatlist Paginated

--- src/models/store.ts
+++ src/models/store.ts
@@ -1,11 +1,43 @@
-import {flow, getSnapshot, Instance, types} from 'mobx-state-tree';
+import {
+  cast,
+  flow,
+  getSnapshot,
+  Instance,
+  toGenerator,
+  types,
+} from 'mobx-state-tree';
 import {createContext} from 'react';
-import flickr from '../api/flickr';
 import {IMAGE_URL, SEARCH_METHOD, INFO_METHOD} from '../constants/urlConstants';
-import {photo} from './photo';
-import {info} from './info';
+import {photo, PhotoType} from './photo';
+import {info, InfoType} from './info';
+import {GetApi} from '../api/api';
 
 export type StoreType = Instance<typeof store>;
+
+export interface Photo {
+  id: string;
+  owner: string;
+  secret: string;
+  server: string;
+  farm: number;
+  title: string;
+  ispublic: number;
+  isfriend: number;
+  isfamily: number;
+}
+
+type SearchApiResponse = {
+  photos: {
+    page: number;
+    pages: number;
+    perpage: number;
+    photo: PhotoType[];
+  };
+};
+
+type InfoApiResponse = {
+  photo: InfoType;
+};
 
 export const store = types
   .model('StoreModel')
@@ -15,7 +47,7 @@ export const store = types
     perpage: types.number,
     total: types.number,
     photosLoading: types.boolean,
-    photos: types.array(photo),
+    photos: types.optional(types.array(photo), []),
     info: types.frozen(
       getSnapshot(
         info.create({notes: {note: []}, tags: {tag: []}, urls: {url: []}}),
@@ -46,21 +78,34 @@ export const store = types
     },
   }))
   .actions(self => ({
-    getPhotos: flow(function* (text: string, perPage: number = 20) {
+    getPhotos: flow(function* (
+      text: string,
+      perPage: number = 20,
+      page: number = 1,
+      newRequest: boolean = false,
+    ) {
       self.photosLoading = true;
-      // const response = yield axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=debb070988cf38ae1960392875f73796&text=${text}&format=json&nojsoncallback=1`);
-      const response = yield flickr.get(`?method=${SEARCH_METHOD}`, {
-        params: {
-          api_key: 'debb070988cf38ae1960392875f73796',
-          text: text,
-          format: 'json',
-          nojsoncallback: 1,
-          per_page: perPage,
-        },
-      });
-      self.photosLoading = false;
-      console.log('PHOTOS : ', response.data.photos);
-      self.photos = response.data.photos.photo;
+      try {
+        const response = yield* toGenerator(
+          GetApi<SearchApiResponse>(SEARCH_METHOD, {
+            api_key: 'debb070988cf38ae1960392875f73796',
+            text: text ? text : 'India',
+            format: 'json',
+            nojsoncallback: 1,
+            per_page: perPage,
+            page: !newRequest ? page : 1,
+          }),
+        );
+        self.photos =
+          self.photos && !newRequest
+            ? cast(self.photos.concat(response.photos.photo))
+            : cast(response.photos.photo);
+        self.page = response.photos.page;
+        self.pages = response.photos.pages;
+        self.photosLoading = false;
+      } catch (err) {
+        self.error = 'Some Thing Unexpected Happened!';
+      }
     }),
     getImageUrl: (
       serverId: string,
@@ -75,16 +120,17 @@ export const store = types
     getImageInfo: flow(function* (photo_id: string, secret: string) {
       self.infoLoading = true;
       try {
-        const response = yield flickr.get(`/?method=${INFO_METHOD}`, {
-          params: {
+        const response = yield* toGenerator(
+          GetApi<InfoApiResponse>(INFO_METHOD, {
             api_key: 'debb070988cf38ae1960392875f73796',
             photo_id,
             secret,
             format: 'json',
             nojsoncallback: 1,
-          },
-        });
-        self.info = response.data.photo;
+          }),
+        );
+        self.info = response.photo;
+
         self.infoLoading = false;
       } catch (err) {
         self.infoLoading = false;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,10 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -6,6 +12,7 @@ import {
   Text,
   View,
   Alert,
+  ActivityIndicator,
 } from 'react-native';
 import type {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStoreParams} from '../navigation/RootNavigator';
@@ -19,20 +26,38 @@ import SearchBar from '../components/SearchBar';
 const Home = observer(() => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStoreParams>>();
-  const {getPhotos, photosCount, photos, photosLoading} =
+  const {getPhotos, photosCount, photos, photosLoading, page, error, pages} =
     useContext(StoreContext);
   const [query, setQuery] = useState('');
+  const flatListRef = useRef();
+
+  const toTop = () => {
+    flatListRef.current.scrollToOffset({animated: true, offset: 0});
+  };
+
+  const loadData = useCallback(() => {
+    if (!photosLoading) {
+      getPhotos(query, 30, undefined, true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
-    getPhotos('india', 30);
-  }, [getPhotos]);
+    loadData();
+
+    return () => loadData();
+  }, [loadData]);
 
   const onSubmit = () => {
     if (query.length > 0) {
-      getPhotos(query);
+      getPhotos(query, 30, undefined, true).then(() => toTop());
     } else {
       Alert.alert('Type SomeThing to Search!');
     }
+  };
+
+  const Footer = () => {
+    return <ActivityIndicator color="black" size={'small'} />;
   };
 
   return (
@@ -45,24 +70,59 @@ const Home = observer(() => {
           onEndEditing={onSubmit}
           placeholder="Search Photos"
         />
-        <Text style={styles.resultsText}>Results Found ({photosCount})</Text>
-        <View style={styles.flatListContainer}>
-          <FlatList
-            data={photos}
-            renderItem={({item}) => {
-              return (
-                <PhotoComponent photo={cast(item)} navigation={navigation} />
-              );
-            }}
-            alwaysBounceVertical={true}
-            ListFooterComponent={() => <Text>End..</Text>}
-            refreshing={photosLoading}
-            onRefresh={() => getPhotos(query.length > 0 ? query : 'India')}
-            contentContainerStyle={styles.flatListContentStyle}
-            showsVerticalScrollIndicator={false}
-            ListFooterComponentStyle={styles.listFooter}
-          />
-        </View>
+        {error ? (
+          <View style={styles.errorContainer}>
+            <Text style={styles.error}>Some Thing Unexpected!</Text>
+          </View>
+        ) : (
+          <>
+            <Text style={styles.resultsText}>
+              Results Found ({photosCount})
+            </Text>
+            <View style={styles.flatListContainer}>
+              <FlatList
+                ref={flatListRef}
+                data={photos}
+                renderItem={({item}) => {
+                  return (
+                    <PhotoComponent
+                      photo={cast(item)}
+                      navigation={navigation}
+                    />
+                  );
+                }}
+                keyExtractor={(item, index) => {
+                  return index.toString();
+                }}
+                alwaysBounceVertical={true}
+                ListFooterComponent={<Footer />}
+                refreshing={photosLoading}
+                onRefresh={() =>
+                  getPhotos(
+                    query.length > 0 ? query : 'India',
+                    30,
+                    undefined,
+                    true,
+                  )
+                }
+                contentContainerStyle={styles.flatListContentStyle}
+                showsVerticalScrollIndicator={false}
+                ListFooterComponentStyle={styles.listFooter}
+                onEndReachedThreshold={0.9}
+                onEndReached={() => {
+                  page !== pages && !photosLoading
+                    ? getPhotos(
+                        query,
+                        30,
+                        page < pages ? page + 1 : page,
+                        false,
+                      )
+                    : null;
+                }}
+              />
+            </View>
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
@@ -88,6 +148,24 @@ const styles = StyleSheet.create({
   },
   listFooter: {
     marginVertical: 10,
+  },
+  error: {
+    color: 'red',
+    fontSize: 16,
+  },
+  errorContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 40,
+  },
+  endContainer: {
+    marginVertical: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  endText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
